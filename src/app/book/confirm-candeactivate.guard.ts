9a11f3b4f4d8ab2c5e2247695686a8d4c490d927
fagherazziC <44192833+fagherazziC@users.noreply.github.com>
2022-08-04 14:41:47 +0200

    delete unused import

--- src/app/book/confirm-candeactivate.guard.ts
+++ src/app/book/confirm-candeactivate.guard.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from '@angular/router';
 import { BookDetailComponent } from '../book-detail/book-detail.component';
 
 
